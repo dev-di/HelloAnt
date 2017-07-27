56960509f09e109b33a9855506e173bdccf5abe9
dev-di <admin@masterin.se>
2017-07-27 17:12:13 +0200

    sysout

--- src/test/MyMathTest.java
+++ src/test/MyMathTest.java
@@ -10,11 +10,13 @@ public class MyMathTest {
     @Test
     public void testMulti() {
         MyMath math = new MyMath();
+        System.out.println("Test A");
         assertEquals(50, math.multi(5, 10));
     }
     @Test
     public void testMultiZero() {
         MyMath math = new MyMath();
+        System.out.println("Test 0");
         assertEquals(0, math.multi(5, 0));
     }
 }
